c7d485d9eb326d9b2fb0a0467b98a1e9cb398ff5
Arnold Saikia <arnoldsaikiaxxar@gmail.com>
2022-11-18 16:19:21 +0530

    Taking text as input and displaying

---
+++ TextInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class TextInput {
+    public static void main(String[] args) {
+        // creating an object of Scanner class
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String myString = input.next();
+        System.out.println("You entered = " + myString);
+
+        // closing the scanner object
+        input.close();
+    }
+}
