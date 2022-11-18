07950e21366f035834a081b916cff8ef686420ff
Arnold Saikia <arnoldsaikiaxxar@gmail.com>
2022-11-18 16:42:12 +0530

    Using for each loop for traversing array

---
+++ ForEach.java
@@ -0,0 +1,11 @@
+class ForEach {
+    public static void main(String[] args) {
+        // create an array
+        int[] numbers = {3, 9, 5, -5};
+
+        // for each loop
+        for(int num: numbers) {
+            System.out.printf("%d\t", num);
+        }
+    }
+}
